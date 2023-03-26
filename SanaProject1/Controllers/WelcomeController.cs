39caba803a2869c828fcd93785a421488320b292
Volkov <volkov.aliksandr0203@gmail.com>
2023-03-27 02:25:03 +0300

    First Project

---
+++ SanaProject1/Controllers/WelcomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SanaProject1.Models;
+
+namespace SanaProject1.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Index(User user)
+        {
+            return View(user);
+        }
+    }
+}
